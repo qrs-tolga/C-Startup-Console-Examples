e9a6a3ce8295eabd65d69c13fcf047f5e484467c
Tolga Tekin <64313872+qrs-tolga@users.noreply.github.com>
2022-11-22 11:45:47 +0300

    Update Program.cs

--- Örnekler/Program.cs
+++ Örnekler/Program.cs
@@ -10,72 +10,72 @@ namespace Örnekler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("İsminizi Girdiğiniz Kadar Yazdıran Program (Soru 1)\n");
-            Console.Write("İsminiz : ");
+            Console.WriteLine("Program That Prints As You Enter Your Name (Example 1)\n");
+            Console.Write("Name : ");
             string isim = Console.ReadLine();
-            Console.Write("İsminiz Kaç Kere Yazılsın : ");
+            Console.Write("How Many Times Should Your Name Be Written : ");
             int tekrar = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 1; i <= tekrar; i++)
             {
                 Console.WriteLine(isim);
             }
-            Console.WriteLine("\n\nDiğerine Geçmek İçin Her Hangi Bir Tuşa Basınız\n\n");
+            Console.WriteLine("\n\nPress Any Key to Switch to Another\n\n");
             Console.ReadKey();
 
 
 
 
-            Console.WriteLine("Hesap Makinesi (Soru 2)\n");
+            Console.WriteLine("Calculator (Example 2)\n");
             int sonuc = 0;
 
-            Console.Write("1. Sayıyı Giriniz : ");
-            int sayi1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("2. Sayıyı Giriniz : ");
-            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("1. Enter Number : ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. Enter Number : ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\nİşlem Seçiniz \n---------------\n + , - , * , /");
+            Console.WriteLine("\nSelect Transaction \n---------------\n + , - , * , /");
             string islem = Console.ReadLine();
 
             if (islem == "+")
             {
-                sonuc = sayi1 + sayi2;
+                sonuc = number1 + number2;
             }
             else if (islem == "-")
             {
-                sonuc = sayi1 - sayi2;
+                sonuc = number1 - number2;
             }
             else if (islem == "*")
             {
-                sonuc = sayi1 * sayi2;
+                sonuc = number1 * number2;
             }
             else if (islem == "/")
             {
-                sonuc = sayi1 / sayi2;
+                sonuc = number1 / number2;
             }
             else
             {
-                Console.WriteLine("Yanlış İşlem Seçtiniz");
+                Console.WriteLine("You Selected Wrong Transaction");
             }
 
             Console.WriteLine("Sonuç : " + sonuc);
-            Console.WriteLine("\n\nDiğerine Geçmek İçin Her Hangi Bir Tuşa Basınız\n\n");
+            Console.WriteLine("\n\nPress Any Key to Switch to Another\n\n");
             Console.ReadKey();
 
 
 
 
 
-            Console.WriteLine("Üçgenin 3. İç Açısını Bulan Program (Soru 3)\n");
-            Console.Write("Üçgenin 1. İç Açısını Giriniz : ");
+            Console.WriteLine("Program to Find the 3rd Interior Angle of a Triangle(Example 3)\n");
+            Console.Write("Enter the 1st Interior Angle of the Triangle : ");
             int icaci1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Üçgenin 2. İç Açısını Giriniz : ");
+            Console.Write("Enter the 2st Interior Angle of the Triangle : ");
             int icaci2 = Convert.ToInt32(Console.ReadLine());
 
             int icaci3 = 180 - icaci1 - icaci2;
 
-            Console.WriteLine("Üçgenin 3. İç Açısı : " + icaci3);
-            Console.WriteLine("\n\nDiğerine Geçmek İçin Her Hangi Bir Tuşa Basınız\n\n");
+            Console.WriteLine("3rd Interior Angle of Triangle : " + icaci3);
+            Console.WriteLine("\n\nPress Any Key to Switch to Another\n\n");
             Console.ReadKey();
         }
     }
